10bfb349d488c4af584bf08ddde9e491896ad72e
M4TH3US17 <matheusdalvino50@gmail.com>
2025-01-28 20:50:24 -0400

    refactor/Limpar Boilerplates

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { MainModule } from './main.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(MainModule);
+  await app.listen(3000);
+}
+bootstrap();
