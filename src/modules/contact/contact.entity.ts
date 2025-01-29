a53945888fe9373f4919a365af1b239f59c06fda
M4TH3US17 <matheusdalvino50@gmail.com>
2025-01-28 22:41:39 -0400

    fix/import base-entity

--- src/modules/contact/contact.entity.ts
+++ src/modules/contact/contact.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity } from "src/shared/bases/base.entity";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name: "contacts"})
 export class ContactEntity {
