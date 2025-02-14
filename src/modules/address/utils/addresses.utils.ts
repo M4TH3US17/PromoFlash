a2269a23d4a8f5c7a839ca4a0dac81a18703e47b
M4TH3US17 <matheusdalvino50@gmail.com>
2025-02-13 21:24:51 -0400

    feat: address utils parses

---
+++ src/modules/address/utils/addresses.utils.ts
@@ -0,0 +1,42 @@
+import { LocationEntity } from "src/modules/location/location.entity";
+import { AddressEntity } from "../address.entity";
+import { CreateAddressRequestDTO } from "../dto/request-address.dto";
+import { AddressResponseDTO } from "../dto/response-address.dto";
+
+
+export function parseAddressRequestToEntity(request: CreateAddressRequestDTO): AddressEntity {
+    const location: LocationEntity = new LocationEntity();
+
+    return {
+        id: null,
+        cep: request.cep,
+        city: request.city,
+        number: request.number,
+        state: request.state,
+        street: request.street,
+        country: request.country,
+        complement: request.complement,
+        neighborhood: request.neighborhood,
+        users: null,
+        establishment: null,
+        location: location,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+};
+
+export function parseAddressEntityToResponse(entity: AddressEntity): AddressResponseDTO {
+    return {
+        id: entity.id,
+        cep: entity.cep,
+        city: entity.city,
+        country: entity.country,
+        state: entity.state,
+        street: entity.street,
+        number: entity.number,
+        complement: entity.complement,
+        neighborhood: entity.neighborhood,
+        createdAt: entity.createdAt,
+        updatedAt: entity.updatedAt,
+    }
+};
