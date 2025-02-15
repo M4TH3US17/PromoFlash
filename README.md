b4d55bc0d3cf37f4310d3b10d01fd5cf97729a32
M4TH3US17 <matheusdalvino50@gmail.com>
2025-02-15 18:58:58 -0400

    feat: swagger config

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ npx typeorm migration:run -d "dist\infrastructure\database\data-source.js"
 
 ## Acessar o PgAdmin:
 http://localhost:5050/
+
+## Acessar Documentacao:
+http://localhost:4000/promoflash-doc
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,28 @@ import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppDataSource } from "./infrastructure/database/data-source";
+import { DocumentBuilder, OpenAPIObject, SwaggerCustomOptions, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
+import { join } from "path";
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(MainModule);
   const APP_PORT: number = Number(process.env.PORT) || 3000;
   const DB_NAME: string = process.env.DB_NAME;
-  
+
   AppDataSource.initialize()
     .then(() => console.log(`Conexão com o banco "${DB_NAME}" foi inicializada!`))
     .catch((error) => console.error(`Conexão com o banco "${DB_NAME}" falhou! Erro:`, error));
 
+  // Swagger Config
+  const swaggerInfos = new DocumentBuilder()
+    .setTitle('Documentação PromoFlash')
+    .setDescription('Documentação da API PromoFlash')
+    .setVersion('1.0')
+    .build()
+  const swaggerUiConfig: SwaggerCustomOptions = { customSiteTitle: "PromoFlash Documentation" };
+  SwaggerModule.setup('promoflash-doc', app, () => SwaggerModule.createDocument(app, swaggerInfos, { autoTagControllers: true }), swaggerUiConfig);
+
   await app.listen(APP_PORT);
-}
+};
 
 bootstrap();
