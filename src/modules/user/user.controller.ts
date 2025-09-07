3c2dad81e36681176fa0e38c5326398f73cadcdc
M4TH3US17 <matheusdalvino50@gmail.com>
2025-09-07 17:34:19 -0300

    verification fix

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -33,7 +33,6 @@ export class UserController {
     public async create(@Body() request: CreateUserRequestDTO, @Res() res: Response) {
         try {
             const data: ResponseUserDTO = await this.service.create(request);
-            console.log(request)
             return res.status(HttpStatus.CREATED).json({
                 message: `Usuário criado com sucesso!`,
                 data: data
